446c3466227d721ffae29265724f0dd65f1395bd
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2023-08-26 14:27:53 +0200

    task one done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const obj = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+  };
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  obj.stopBtn.setAttribute('disabled', true)
+  obj.startBtn.addEventListener('click', clickStartBtn)
+  obj.stopBtn.addEventListener('click', clickStopBtn)
+  
+  let intervalId;
+
+  function clickStartBtn (e){
+//e.target dotyczy startBtn
+e.target.setAttribute('disabled', true)
+intervalId = setInterval(bodyColorChange, 1000)
+obj.stopBtn.removeAttribute('disabled')
+
+  }
+  function bodyColorChange (){
+    document.body.style.backgroundColor = getRandomHexColor();
+  }
+
+  function clickStopBtn (e){
+    clearInterval(intervalId)
+    obj.startBtn.removeAttribute('disabled')
+    e.target.setAttribute('disabled', true)
+  }
