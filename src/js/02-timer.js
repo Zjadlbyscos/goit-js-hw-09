ee6c343069ef78ad32c662aed6d96f555a98d415
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2023-08-26 17:58:27 +0200

    task 2 changes about counting-down

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,44 +5,47 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
 
 const obj = {
-dateTimePicker: document.querySelector('#datetime-picker'),
-startButton: document.querySelector('[data-start]'),
-time:{
-days: document.querySelector('[data-days]'),
-hours: document.querySelector('[data-hours]'),
-minutes: document.querySelector('[data-minutes]'),
-seconds: document.querySelector('[data-seconds]'),
-}
-}
+  dateTimePicker: document.querySelector('#datetime-picker'), // Poprawione
+  startButton: document.querySelector('[data-start]'),
+  time: {
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]'),
+  },
+};
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+   datePicker(selectedDates); 
+   console.log(selectedDates[0]);
+  },
+};
 
-  obj.startButton.setAttribute('disabled', true);
+obj.startButton.setAttribute('disabled', true);
 
-flatpickr(obj.dateTimePicker,{...options});
+flatpickr(obj.dateTimePicker, { ...options });
 
 function datePicker(selectedDates) {
-    // why why ?
-    if (selectedDates[0].getTime()<= new Date()){
-        Notify.failure('Please choose a date in the future',{
-            clickToClose: true,
-            timeout: 10 * 1000,
-            position: 'top-right',
-        });
-        obj.startButton.setAttribute('disabled', true);
-    }else{
-        obj.startButton.setAttribute('disabled', false);
-    }
+  if (selectedDates[0].getTime() <= new Date().getTime()) {
+    Notify.failure('Please choose a date in the future', {
+      clickToClose: true,
+      timeout: 3 * 1000,
+      position: 'center-top',
+    });
+    obj.startButton.setAttribute('disabled', true);
+  } else {
+    obj.startButton.setAttribute('disabled', false);
+  }
 }
+
 obj.startButton.addEventListener('click', btnStartHandler);
+
+let timerId; 
 
 function btnStartHandler(){
     obj.startButton.setAttribute('disabled', true);
@@ -67,26 +70,20 @@ timerId = setInterval(() => {
 function addLeadingZero(value) {
     return value.toString().padStart(2, '0');
 }
-
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-      const second = 1000;
-      const minute = second * 60;
-      const hour = minute * 60;
-      const day = hour * 24;
-    
-    // Remaining days
-      const days = Math.floor(ms / day);
-    // Remaining hours
-      const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-      const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-    
-      return { days, hours, minutes, seconds };
-    }
-    
-    console.log(convertMs(2000));// {days: 0, hours: 0, minutes: 0, seconds: 2}
-    console.log(convertMs(140000));// {days: 0, hours: 0, minutes: 2, seconds: 20}
-    console.log(convertMs(24140000));// {days: 0, hours: 6 minutes: 42, seconds: 20}
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000));
+console.log(convertMs(140000));
+console.log(convertMs(24140000));
